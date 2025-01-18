a45597e4519fe220f41a784f71968fdc9203efda
Deepak Soni <deepaksonii@outlook.in>
2025-01-19 01:39:29 +0530

    improvements

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
 install:
 	pip install -r requirements.txt
 	spacy download en_core_web_sm
-
-	rm -rf ./uploads
--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, request
+import uuid
+import shutil
+import os
+from screener.screener import screen
 
 # Constants
 DEBUG: bool = True
+UPLOAD_DIR: bool = "./uploads"
+DEFAULT_APPLICANTS_TOP_K: int = 3
+
+# Remove Uploads
+print("Removing uploads...")
+for each in os.listdir(UPLOAD_DIR):
+    try: os.remove(f"{UPLOAD_DIR}/{each}") if os.path.isfile(each) else shutil.rmtree(f"{UPLOAD_DIR}/{each}")
+    except: print(f"Failed to remove: {UPLOAD_DIR}/{each}")
+
+print()
 
 # Initialize Flask App
 app: Flask = Flask(__name__, template_folder="templates")
@@ -22,7 +36,41 @@ def screen_route() -> None:
     Screens resumes and returns the results.
     """
 
-    print([file.save(f"./uploads/{file.filename}") for file in request.files.getlist("file")])
+    # Get Form Data
+    preferences: str = request.form.get("preferences").strip()
+    top_k: int = 0
+
+    try: top_k: int = int(request.form.get("top_k"))
+    except ValueError: pass
+
+    # Create Unique Uploads Sub-Dir
+    uploads_subdir: str = str(uuid.uuid4())  # Generate UUID
+    os.mkdir(f"{UPLOAD_DIR}/{uploads_subdir}")  # Create Directory
+
+    resumes: list[str] = []
+    applicants: list[str] = []
+
+    # Save Each File in Uploads Sub-Directory
+    for file in request.files.getlist("file"):
+        resume: str = f"{UPLOAD_DIR}/{uploads_subdir}/{file.filename}"  # Path Includes 'Uploads Sub-Directory'
+
+        resumes.append(resume)  # Record Resume Path
+        applicants.append(file.filename.rsplit(".", maxsplit=1)[0])  # Record Applicant
+
+        file.save(resume)
+
+    # Compute Results
+    results: list[tuple[str, float]] = screen(
+        resumes=resumes,
+        applicants=applicants,
+        preferences=preferences,
+        top_k=top_k if top_k > 0 else DEFAULT_APPLICANTS_TOP_K
+    )
+
+    print(results)  # TODO: Remove
+
+    # Delete Uploads Sub-Directory
+    shutil.rmtree(f"{UPLOAD_DIR}/{uploads_subdir}")
 
     return render_template("result.html")
 
